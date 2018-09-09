96dbc55f458f235699994730e4940e4c3a921fd2
Roar <z1991998920@gmail.com>
2018-09-09 15:09:08 +0800

    modify reqirements & add coding style

--- biohub/biohub/settings.py
+++ biohub/biohub/settings.py
@@ -38,8 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'accounts',
+    # 'accounts',
     'user',
+    'report',
 ]
 
 MIDDLEWARE = [
--- biohub/user/views.py
+++ biohub/user/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import User
 
 # Create your views here.
+
+def get_users(request):
+    if request.method == 'GET':
+        users = User.objects.all()
