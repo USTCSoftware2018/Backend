3e5491b30824dcc1962aeedd76ac5ad716440fea
Roar <z1991998920@gmail.com>
2018-09-11 21:41:05 +0800

    add_report_component_models

--- biohub/report/models.py
+++ biohub/report/models.py
@@ -43,3 +43,42 @@ class CommentReply(Comment):
                                     related_name='replied_by')
     super_comment = models.ForeignKey(Comment, on_delete=models.CASCADE, default=None, blank=True, null=True,
                                       related_name='sub_comments')
+
+
+# Edit process
+class Component(models.Model):
+    icon = models.URLField()
+    name = models.CharField(max_length=64)
+    description = models.TextField()
+    template = models.TextField()
+    users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='collected_steps')
+    created_user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='created_steps', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return 'id:{}, name: {}'.format(self.pk, self.name)
+
+
+class SubRoutine(models.Model):
+    icon = models.URLField()
+    name = models.CharField(max_length=64)
+    description = models.TextField()
+    default = models.TextField()
+    data = models.ManyToManyField(Component, through='SubroutingComponent')
+
+    def data_list(self):
+        return [sc.component for sc in SubroutingComponent.objects.filter(sub_routine=self).order_by('order')]
+
+    def __str__(self):
+        return 'id:{}, name:{}'.format(self.pk, self.name)
+
+
+class SubroutingComponent(models.Model):
+    component = models.ForeignKey(Component, on_delete=models.CASCADE)
+    sub_routine = models.ForeignKey(SubRoutine, on_delete=models.CASCADE, db_index=True)
+    order = models.IntegerField()
+
+    class Meta:
+        ordering = ['order', ]
+
+    def __str__(self):
+        return 'comp:{}, subr:{}, no:{}'.format(self.component.name, self.sub_routine.name, self.order)
