40cddeb881385b28ec974579ddeb1bf73f00218c
Roar <z1991998920@gmail.com>
2018-09-11 22:55:50 +0800

    modify_report_component_models

--- biohub/report/models.py
+++ biohub/report/models.py
@@ -10,6 +10,7 @@ class Report(models.Model):
     labels = models.ManyToManyField('Label', related_name='reports_related')
     pub_time = models.DateTimeField(auto_now_add=True)
     latest_edit_time = models.DateTimeField(auto_now=True)
+
     # See comments in Comment model!
     # See praises(likes in the doc) in User model
 
@@ -22,6 +23,10 @@ class Label(models.Model):
 
     def __str__(self):
         return 'id:{}, name:{}'.format(self.pk, self.label_name)
+
+
+class Graph(models.Model):
+    graph = models.ImageField(upload_to='report_graph', null=True, blank=True)
 
 
 class Comment(models.Model):
@@ -53,28 +58,55 @@ class Component(models.Model):
     template = models.TextField()
     users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='collected_steps')
     created_user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='created_steps', on_delete=models.CASCADE)
+    default = models.TextField(default='', null=True, blank=True)
 
     def __str__(self):
         return 'id:{}, name: {}'.format(self.pk, self.name)
 
 
 class SubRoutine(models.Model):
+    STEP = 'ST'
+    INFO = 'IN'
+    RESULT = 'RE'
+    PICTURES = 'PI'
+    QUOTE = 'QU'
+    TEXT = 'TE'
+    TABLE = 'TA'
+    rt_type_choices = (
+        (STEP, 'Steps'),
+        (INFO, 'Info'),
+        (RESULT, 'Result'),
+        (PICTURES, 'Pictures'),
+        (QUOTE, 'Quote'),
+        (TEXT, 'Text'),
+        (TABLE, 'Table'),
+    )
+    rt_type = models.CharField(max_length=2, choices=rt_type_choices, null=False, blank=False)
+    ##########################
+
+
+class BaseSubRoutine(models.Model):
+    step_num = models.IntegerField()
     icon = models.URLField()
     name = models.CharField(max_length=64)
     description = models.TextField()
-    default = models.TextField()
-    data = models.ManyToManyField(Component, through='SubroutingComponent')
-
-    def data_list(self):
-        return [sc.component for sc in SubroutingComponent.objects.filter(sub_routine=self).order_by('order')]
 
     def __str__(self):
         return 'id:{}, name:{}'.format(self.pk, self.name)
 
 
+# MainSubRoutine
+class MainSubRoutine(BaseSubRoutine):
+    data = models.ManyToManyField(Component, through='SubroutingComponent')
+
+    def data_list(self):
+        return [sc.component for sc in SubroutingComponent.objects.filter(sub_routine=self).order_by('order')]
+
+
 class SubroutingComponent(models.Model):
     component = models.ForeignKey(Component, on_delete=models.CASCADE)
-    sub_routine = models.ForeignKey(SubRoutine, on_delete=models.CASCADE, db_index=True)
+    sub_routine = models.ForeignKey(MainSubRoutine, on_delete=models.CASCADE, db_index=True)
+    component_default_json = models.TextField()
     order = models.IntegerField()
 
     class Meta:
@@ -82,3 +114,11 @@ class SubroutingComponent(models.Model):
 
     def __str__(self):
         return 'comp:{}, subr:{}, no:{}'.format(self.component.name, self.sub_routine.name, self.order)
+
+
+# ResultSubRoutine
+class ResultSubRoutine(BaseSubRoutine):
+    Result = models.TextField()
+
+# 写各类subroutine 包括67行的所有
+
