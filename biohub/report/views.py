0712e062256d2f33dc63a2eb07ca9d565ee9fdcd
Roar <z1991998920@gmail.com>
2018-09-12 19:04:28 +0800

    add_user_validation

--- biohub/report/views.py
+++ biohub/report/views.py
@@ -13,60 +13,61 @@ from user.models import User
 
 def post_picture(request):
     if request.method == 'POST' or 'OPTIONS':
-        user = User.objects.filter(pk=1)[0]
+        user_pk = request.user.pk
+        user = User.objects.get(pk=user_pk)
         uidb64 = bytes.decode(urlsafe_base64_encode(force_bytes(user.pk)))
-        # if user and user.is_active:
-        try:
-            picture = request.FILES.get('file')
-        except:
-            err_msg = {
-                'meta': {
-                    'success': False,
-                    'message': 'No Picture Updated',
-                },
-                'data': None
-            }
-            respone = HttpResponse(content_type="application/json")
-            respone.write(json.dumps(err_msg))
-            return respone
+        if user and user.is_active:
+            try:
+                picture = request.FILES.get('file')
+            except:
+                err_msg = {
+                    'meta': {
+                        'success': False,
+                        'message': 'No Picture Updated',
+                    },
+                    'data': None
+                }
+                response = HttpResponse(content_type="application/json")
+                response.write(json.dumps(err_msg))
+                return response
 
-        if 10000 < picture.size < 409600000:
-            picture.name = uidb64 + '_' + timezone.now().strftime('%Y%m%d%H%M%S') + '_' + picture.name
-            image = Graph(owner=user, graph=picture)
-            image.save()
-            msg = {
-                'meta': {
-                    'success': True,
-                    'message': 'Success',
-                },
-                'data': settings.MEDIA_ROOT + image.graph.url
-            }
-            respone = HttpResponse(content_type="application/json")
-            respone.write(json.dumps(msg))
-            return respone
-            # return HttpResponse(json.dumps(msg))
+            if 10000 < picture.size < 409600000:
+                picture.name = uidb64 + '_' + timezone.now().strftime('%Y%m%d%H%M%S') + '_' + picture.name
+                image = Graph(owner=user, graph=picture)
+                image.save()
+                msg = {
+                    'meta': {
+                        'success': True,
+                        'message': 'Success',
+                    },
+                    'data': settings.MEDIA_ROOT + image.graph.url
+                }
+                response = HttpResponse(content_type="application/json")
+                response.write(json.dumps(msg))
+                return response
+                # return HttpResponse(json.dumps(msg))
+            else:
+                err_msg = {
+                    'meta': {
+                        'success': False,
+                        'message': 'Picture Is Too Large Or Too Small',
+                    },
+                    'data': str(picture.size/1000) + 'kb'
+                }
+                response = HttpResponse(content_type="application/json")
+                response.write(json.dumps(err_msg))
+                return response
         else:
             err_msg = {
                 'meta': {
                     'success': False,
-                    'message': 'Picture Is Too Large Or Too Small',
+                    'message': 'Identify Error',
                 },
-                'data': str(picture.size/1000) + 'kb'
+                'data': None,
             }
-            respone = HttpResponse(content_type="application/json")
-            respone.write(json.dumps(err_msg))
-            return respone
-        # else:
-        #     err_msg = {
-        #         'meta': {
-        #             'success': False,
-        #             'message': 'Identify Error',
-        #         },
-        #         'data': None,
-        #     }
-        #     respone = HttpResponse(content_type="application/json")
-        #     respone.write(json.dumps(err_msg))
-        #     return respone
+            response = HttpResponse(content_type="application/json")
+            response.write(json.dumps(err_msg))
+            return response
     else:
         err_msg = {
             'meta': {
@@ -75,8 +76,8 @@ def post_picture(request):
             },
             'data': None,
         }
-        respone = HttpResponse(content_type="application/json")
-        respone.write(json.dumps(err_msg))
-        return respone
+        response = HttpResponse(content_type="application/json")
+        response.write(json.dumps(err_msg))
+        return response
 
 
